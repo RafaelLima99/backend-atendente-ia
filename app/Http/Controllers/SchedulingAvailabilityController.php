d7271d1213769a5b2b17e3d411cbbc8741746b93
Rafael Lima <rafael.lima@unu.com.br>
2026-06-09 09:17:34 -0300

    feat: criação das migrations e adição dos aquivos controllers e models

---
+++ app/Http/Controllers/SchedulingAvailabilityController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\scheduling_availability;
+use Illuminate\Http\Request;
+
+class SchedulingAvailabilityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(scheduling_availability $scheduling_availability)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(scheduling_availability $scheduling_availability)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, scheduling_availability $scheduling_availability)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(scheduling_availability $scheduling_availability)
+    {
+        //
+    }
+}
